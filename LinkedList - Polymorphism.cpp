9763f38c0e1610ff902ee50ddadafe5637d9d195
Kanwar Adnan <107979019+KanwarAdnan@users.noreply.github.com>
2022-09-05 14:50:24 +0500

    Removed Bug in InsertAtLast

--- LinkedList - Polymorphism.cpp
+++ LinkedList - Polymorphism.cpp
@@ -26,11 +26,12 @@ protected:
     Node * head = NULL;
     int length;
 public:
-    int getLength()                 { return this->length; }
-    bool isEmpty()                  { return this->head == NULL; }
+    int getLength()             { return this->length; }
+    bool isEmpty()              { return this->head == NULL; }
     // virtual functions
     virtual void insertAtHead(int) = 0;
     virtual void insertAtLast(int) = 0;
+    virtual void removeAtHead() = 0;
     virtual void print() = 0;
     virtual Node * getHead() = 0;
 
@@ -38,7 +39,7 @@ public:
 
 class Singly : public LinkedList{
 public:
-    Node * getHead()                { return this->head; }
+    Node * getHead()            { return this->head; }
 
     void insertAtHead(int value){
         Node * temp = new Node(value,this->head);
@@ -52,9 +53,9 @@ public:
             this->head = temp;
         }
         else {
-            Node * previous = NULL; // second Last ptr
-            Node * current = this->head;
-            while (current->getNext() != NULL){
+            Node * previous = NULL; // Last ptr
+            Node * current = this->head; // null pointer in the end
+            while (current != NULL){
                 previous = current;
                 current = current->getNext();
             }
@@ -62,10 +63,25 @@ public:
         }
         this->length++;
     }
+
+    void removeAtHead(){
+        if (!(this->isEmpty())){
+            if (this->getLength() == 1){
+                delete this->head;
+                this->head = NULL;
+            }
+            else {
+                Node * temp = this->head; // to delete
+                this->head = this->head->getNext();
+                delete temp;
+            }
+            this->length--;
+        }
+    }
     
     void print(){
         Node * temp = this->head;
-        while (temp){
+        while (temp != NULL){
             cout << temp->getData() << "->";
             temp = temp->getNext();
         }
@@ -77,13 +93,12 @@ public:
 
 int main(){
     LinkedList * l1 = new Singly;
-    l1->insertAtLast(3);
+    l1->insertAtLast(2);
+    l1->insertAtLast(2);
+    l1->insertAtLast(2);
     l1->insertAtHead(1);
-    l1->insertAtHead(1);
-    l1->insertAtHead(1);
-    l1->insertAtHead(2);
     l1->print();
-    cout << l1->getLength();
+    cout << "SLL Length : " << l1->getLength();
     cout << endl;
     return 0;
 }
