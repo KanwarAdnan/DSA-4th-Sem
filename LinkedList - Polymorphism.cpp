cce9a2e75df30c73ac304c550254f9983a615d18
Kanwar Adnan <107979019+KanwarAdnan@users.noreply.github.com>
2022-09-05 14:34:45 +0500

    Added InsertAtLast and updated the format

--- LinkedList - Polymorphism.cpp
+++ LinkedList - Polymorphism.cpp
@@ -2,79 +2,88 @@
 using namespace std;
 
 class Node{
-    private:
-        int data;
-        Node * next;
-        Node * prev;
-    public:
-        Node(int data = 0 , Node * next = NULL , Node * prev = NULL){
-            this->data = data;
-            this->next = next;
-            this->prev = prev;
-        }
-        
-        int getData(){
-            return this->data;
-        }
-        
-        void setData(int data){
-            this->data = data;
-        }
-        
-        void setPrev(Node * prev){
-            this->prev = prev;
-        }
-        
-        void setNext(Node * next){
-            this->next = next;
-        }
-        
-        Node * getPrev(){
-            return this->prev;
-        }
-        
-        Node * getNext(){
-            return this->next;
-        }
-
+private:
+    int data;
+    Node * next;
+    Node * prev;
+public:
+    Node(int data = 0 , Node * next = NULL , Node * prev = NULL){
+        this->data = data;
+        this->next = next;
+        this->prev = prev;
+    }
     
+    void setData(int data)      { this->data = data; }
+    void setPrev(Node * prev)   { this->prev = prev; }
+    void setNext(Node * next)   { this->next = next; }
+    int getData()               { return this->data; }
+    Node * getPrev()            { return this->prev; }
+    Node * getNext()            { return this->next; }
 };
 
 class LinkedList{
-    protected:
-        Node * head = NULL;
-        int length;
-    public:
-        virtual void insertAtHead(int) = 0;
-        virtual void print() = 0;
+protected:
+    Node * head = NULL;
+    int length;
+public:
+    int getLength()                 { return this->length; }
+    bool isEmpty()                  { return this->head == NULL; }
+    // virtual functions
+    virtual void insertAtHead(int) = 0;
+    virtual void insertAtLast(int) = 0;
+    virtual void print() = 0;
+    virtual Node * getHead() = 0;
+
 };
 
 class Singly : public LinkedList{
-    public:
-        void insertAtHead(int x){
-            Node * temp = new Node(x,this->head);
+public:
+    Node * getHead()                { return this->head; }
+
+    void insertAtHead(int value){
+        Node * temp = new Node(value,this->head);
+        this->head = temp;
+        this->length++;
+    }
+    
+    void insertAtLast(int value){
+        Node * temp = new Node(value);
+        if (this->isEmpty()){
             this->head = temp;
-            this->length++;
         }
-        
-        void print(){
-            Node * temp = this->head;
-            while (temp){
-                cout << temp->getData() << "->";
-                temp = temp->getNext();
+        else {
+            Node * previous = NULL; // second Last ptr
+            Node * current = this->head;
+            while (current->getNext() != NULL){
+                previous = current;
+                current = current->getNext();
             }
-            cout << "NULL\n";
+            previous->setNext(temp);
         }
+        this->length++;
+    }
+    
+    void print(){
+        Node * temp = this->head;
+        while (temp){
+            cout << temp->getData() << "->";
+            temp = temp->getNext();
+        }
+        cout << "NULL\n";
+    }
+    
     
 };
 
-
 int main(){
     LinkedList * l1 = new Singly;
+    l1->insertAtLast(3);
     l1->insertAtHead(1);
     l1->insertAtHead(1);
     l1->insertAtHead(1);
-    l1->insertAtHead(1);
+    l1->insertAtHead(2);
     l1->print();
+    cout << l1->getLength();
+    cout << endl;
     return 0;
 }
